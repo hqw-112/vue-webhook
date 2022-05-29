7da9b00c7be5777d88eac65e6ef1274172611933
hqw-112 <836861836@qq.com>
2022-05-30 03:34:56 +0800

    f

---
+++ bvideo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+WORK_PATH='/usr/projects/bvideo'
+cd $WORK_PATH
+echo "清理代码"
+git reset --hard master
+git clean -f
+echo "拉取最新代码"
+git pull master
+echo "开始构建镜像"
+docker build -t bvideo:1.0 .
+echo "删除旧容器"
+docker stop bvideo-container
+docker rm bvideo-container
+echo "启动新容器"
+docker container run -p 9000:9000 -d --name bvideo-container bvideo:1.0
---
+++ fvideo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+WORK_PATH='/usr/projects/fvideo'
+cd $WORK_PATH
+echo "清理代码"
+git reset --hard master
+git clean -f
+echo "拉取最新代码"
+git pull master
+echo "打包最新代码"
+npm run build
+echo "开始构建镜像"
+docker build -t fvideo:1.0 .
+echo "删除旧容器"
+docker stop fvideo-container
+docker rm fvideo-container
+echo "启动新容器"
+docker container run -p 80:80 -d --name fvideo-container fvideo:1.0
